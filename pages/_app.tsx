5fb36442e65af647476c20bcb6fe77cd11256492
Rene UX_unicorn van Dijk <renevandijk@MacBook-Pro-van-Rene.local>
2021-11-13 20:52:18 +0100

    add apollo client, jest, prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
-import "tailwindcss/tailwind.css";
-import type { AppProps } from "next/app";
-import { RecoilRoot } from "recoil";
+import 'tailwindcss/tailwind.css';
+import type { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
+import { ApolloProvider } from '@apollo/client';
+import { client } from '../graphql/mainClient';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <Component {...pageProps} />
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
     </RecoilRoot>
   );
 }
